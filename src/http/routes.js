5fb0259573db5d26fa51532fc05bf7d80fe88574
danielmanfred <danielmanfred@gmail.com>
2018-06-25 01:06:33 -0300

    CRUD da categoria

--- src/http/routes.js
+++ src/http/routes.js
@@ -16,18 +16,28 @@ const routes = (server) => {
     }
   })
 
-  server.post('/category', (req, res, next) => {
+  server.post('/category', async (req, res, next) => {
+    const { id, name } = req.params
+    try {
+      res.send(await db.categories().save(id, name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
+  })
+
+  server.put('/category', async (req, res, next) => {
     const { name } = req.params
-    res.send(name)
-    next()
+    try {
+      res.send(await db.categories().update(name))
+      next()
+    } catch (error) {
+      res.send(error)
+      next()
+    }
   })
-
   /*
-  server.put('/category', (req, res, next) => {
-    res.send()
-    next()
-  })
-
   server.delete('/category', (req, res, next) => {
     res.send()
     next()
--- src/services/mysql/categories.js
+++ src/services/mysql/categories.js
@@ -12,8 +12,30 @@ const categories = deps => {
         })
       })
     },
-    save: (name) => {},
-    update: (id, name) => {},
+    save: (name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('INSERT INTO categories (name) VALUES (?)', [name], (error, results) => {
+          if (error) {
+            errorHandler(error, `Failed to save the categories ${name}`, reject)
+            return false
+          }
+          resolve({ category: { name, id: results.insertId } })
+        })
+      })
+    },
+    update: (id, name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps
+        connection.query('UPDATE categories SET name = ? WHERE id = ?', [name, id], (error, results) => {
+          if (error) {
+            errorHandler(error, `Failed to update the categories ${name}`, reject)
+            return false
+          }
+          resolve({ category: { name, id } })
+        })
+      })
+    },
     del: (id) => {}
   }
 }
